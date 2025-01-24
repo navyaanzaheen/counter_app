e42d481641670781f374f06d86c3f9c8a21ccb8d
navyaanzaheen <navyaanzaheen@gmail.com>
2025-01-24 16:00:07 +0500

    Add files via upload

---
+++ counter-app/src/components/Counter.js
@@ -0,0 +1,98 @@
+import React,{useState} from 'react'
+import '../App.css'
+
+
+
+
+export default function Counter() { 
+
+const [count,setCount] = useState(0);
+
+
+
+
+const increment =() =>{
+
+    setCount(count + 1);
+       
+}
+
+
+
+
+const decrement =() =>{
+
+    setCount(count - 1);
+    
+}
+
+
+const reset = () =>{
+
+  setCount(0);  
+    
+}
+
+
+
+
+const startValue = () => {
+  let div1 = document.createElement("div");
+  div1.style.textAlign = 'center';
+
+  let inp = document.createElement("input");
+
+  inp.id = "inp1";
+  div1.appendChild(inp);
+
+  let btn = document.createElement("button");
+  btn.textContent = "set";
+  btn.addEventListener('click', () => { 
+    let val = document.getElementById('inp1');
+    let value = parseInt(inp.value);
+         
+        document.getElementById('num').value = val;
+        setCount(value);
+        inp.style.display = "none";
+        btn.style.display = "none";
+  });
+  div1.appendChild(btn);
+
+  document.body.appendChild(div1);
+};
+
+
+
+
+
+
+
+
+
+
+return (
+
+<>    
+<div className='container'>
+
+<h1>Counter</h1>
+
+
+
+
+<input type="number" id="num" value={count} 
+  onChange={(e) => setCount(parseInt(e.target.value))}/>
+
+
+<div className="buttons">
+
+<button type="button"onClick={decrement}>-</button>    
+<button type="button" onClick={increment}>+</button>    
+<button type="button" onClick={startValue}>Start Value </button>
+<button type="button" onClick={reset}>Reset Counter</button>
+</div>
+</div>
+</>
+)
+
+}
